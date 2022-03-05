f49400f5b401d5210ad9a3d0244f5ab168a573fa
Carlos Chininin <carloschininin@gmail.com>
2022-03-05 12:19:30 -0500

    add config bundle

---
+++ src/Infrastructure/Symfony/AppBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the PIDIA
+ * (c) Carlos Chininin <cio@pidia.pe>
+ */
+
+namespace CarlosChininin\App\Infrastructure\Symfony;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class AppBundle extends Bundle
+{
+}
---
+++ src/Infrastructure/Symfony/DependencyInjection/AppExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the PIDIA
+ * (c) Carlos Chininin <cio@pidia.pe>
+ */
+
+namespace CarlosChininin\App\Infrastructure\Symfony\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class AppExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        var_dump('We\'re alive!');
+        exit;
+    }
+}
