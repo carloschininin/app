f18c694ff70f1c613fd034f9843fe9933c66312d
Carlos Chininin <carloschininin@gmail.com>
2022-03-13 14:57:29 -0500

    add export data

---
+++ src/Infrastructure/Manager/ExportManager.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CarlosChininin\App\Infrastructure\Manager;
+
+use CarlosChininin\Data\Export\ExportExcel;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ExportManager extends BaseManager
+{
+    public function execute(array $items, array $headers, string $fileName = 'export', array $options = []): Response
+    {
+        $export = new ExportExcel($items, $headers, $options);
+        $export->execute()->headerStyle()->columnAutoSize();
+
+        return $export->download($fileName);
+    }
+}
