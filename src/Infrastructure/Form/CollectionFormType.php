b43824fc229d6252bd1a15a0069f887e898dd0bc
Carlos Chininin <carloschininin@gmail.com>
2022-10-26 22:42:54 -0500

    add collection form

---
+++ src/Infrastructure/Form/CollectionFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the PIDIA
+ * (c) Carlos Chininin <cio@pidia.pe>
+ */
+
+namespace CarlosChininin\App\Infrastructure\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CollectionFormType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return CollectionType::class;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'collections';
+    }
+}
