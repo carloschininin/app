ebe4adf58305b6298389d7c5ae58f3daff690fed
Carlos Chininin <carloschininin@gmail.com>
2022-10-30 12:08:25 -0500

    add number decimal type form

---
+++ src/Infrastructure/Form/NumberDecimalType.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the PIDIA
+ * (c) Carlos Chininin <cio@pidia.pe>
+ */
+
+namespace CarlosChininin\App\Infrastructure\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\DataTransformer\NumberToLocalizedStringTransformer;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NumberDecimalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->resetViewTransformers()
+            ->addViewTransformer(new NumberToLocalizedStringTransformer(
+                $options['scale'],
+                $options['grouping'],
+                $options['rounding_mode'],
+                'en'
+            ));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+
+        $view->vars['decimal_separator'] = $options['decimal_separator'];
+        $view->vars['group_separator'] = $options['group_separator'];
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults([
+            'decimal_separator' => null,
+            'group_separator' => null,
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return NumberType::class;
+    }
+}
