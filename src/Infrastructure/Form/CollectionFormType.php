d0b27ad1da5c4802a0380ab5aecf95c1da0912d2
Carlos Chininin <carloschininin@gmail.com>
2024-05-22 00:11:40 -0500

    add custom button collection form

--- src/Infrastructure/Form/CollectionFormType.php
+++ src/Infrastructure/Form/CollectionFormType.php
@@ -11,13 +11,25 @@ namespace CarlosChininin\App\Infrastructure\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CollectionFormType extends AbstractType
 {
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $view->vars['btn_title'] = $options['btn_title'] ?? null;
+        $view->vars['btn_top'] = $options['btn_top'];
+        $view->vars['add_last'] = $options['add_last'];
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'btn_title' => null,
+            'btn_top' => false,
+            'add_last' => true,
         ]);
     }
 
