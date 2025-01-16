177dcab634750f3478d82838ac0c6dbdd5e10d87
Carlos Chininin <carloschininin@gmail.com>
2025-01-16 01:50:57 -0500

    add datepicker form

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,84 +14,24 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__);
 
 return (new PhpCsFixer\Config())
+    ->setUsingCache(true)
     ->setRiskyAllowed(true)
-    ->setRules(
-        [
-            '@Symfony' => true,
-            '@Symfony:risky' => true,
-            'array_syntax' => [
-                'syntax' => 'short',
-            ],
-            'native_constant_invocation' => [
-                'fix_built_in' => false,
-            ],
-            'combine_consecutive_issets' => true,
-            'combine_consecutive_unsets' => true,
-            'escape_implicit_backslashes' => true,
-            'explicit_indirect_variable' => true,
-            'final_internal_class' => true,
-            'fopen_flags' => [
-                'b_mode' => true,
-            ],
-            'header_comment' => [
-                'header' => $headerComment,
-                'separate' => 'both',
-            ],
-            'heredoc_to_nowdoc' => true,
-            'linebreak_after_opening_tag' => true,
-            'list_syntax' => [
-                'syntax' => 'short',
-            ],
-            'mb_str_functions' => true,
-            'multiline_whitespace_before_semicolons' => [
-                'strategy' => 'no_multi_line',
-            ],
-            'no_php4_constructor' => true,
-            'echo_tag_syntax' => true,
-            'no_unreachable_default_argument_value' => true,
-            'no_useless_else' => true,
-            'no_useless_return' => true,
-            'ordered_class_elements' => [
-                'order' => [
-                    'use_trait',
-                    'constant_public',
-                    'constant_protected',
-                    'constant_private',
-                    'property_public',
-                    'property_protected',
-                    'property_private',
-                    'construct',
-                    'destruct',
-                    'magic',
-                    'phpunit',
-                    'method_public',
-                    'method_protected',
-                    'method_private',
-                ],
-            ],
-            'ordered_imports' => true,
-            'php_unit_strict' => true,
-            'php_unit_no_expectation_annotation' => true,
-            'php_unit_test_class_requires_covers' => true,
-            'phpdoc_order' => true,
-            'phpdoc_summary' => false,
-            'psr_autoloading' => true,
-            'semicolon_after_instruction' => true,
-            'strict_comparison' => true,
-            'strict_param' => true,
-            'declare_strict_types' => true,
-            'class_attributes_separation' => [
-                'elements' => [
-                    'const' => 'none',
-                    'method' => 'one',
-                    'property' => 'none',
-                    'trait_import' => 'none',
-                ],
-            ],
-            'method_argument_space' => [
-                'on_multiline' => 'ensure_fully_multiline',
-            ],
-        ]
-    )
     ->setFinder($finder)
-    ->setCacheFile(__DIR__.'.php_cs.cache');
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'header_comment' => ['header' => $headerComment, 'separate' => 'both'],
+        'linebreak_after_opening_tag' => true,
+        'mb_str_functions' => true,
+        'no_php4_constructor' => true,
+        'no_unreachable_default_argument_value' => true,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'php_unit_strict' => true,
+        'phpdoc_order' => true,
+        'strict_comparison' => true,
+        'strict_param' => true,
+        'blank_line_between_import_groups' => false,
+        'declare_strict_types' => true,
+    ])
+    ->setCacheFile(__DIR__.'/var/.php-cs-fixer.cache');
---
+++ src/Infrastructure/Form/DatePickerType.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the PIDIA
+ * (c) Carlos Chininin <cio@pidia.pe>
+ */
+
+namespace CarlosChininin\App\Infrastructure\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class DatePickerType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'widget' => 'single_text',
+            'format' => 'dd/MM/yyyy',
+            'html5' => false,
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return DateType::class;
+    }
+}
