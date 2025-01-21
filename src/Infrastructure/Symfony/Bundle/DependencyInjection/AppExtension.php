a6d4a2646b5d1dc17691803ee755bf3c0a9281d6
Carlos Chininin <carloschininin@gmail.com>
2025-01-20 22:22:13 -0500

    fix Extension Kernel Deprecated

--- src/Infrastructure/Symfony/Bundle/DependencyInjection/AppExtension.php
+++ src/Infrastructure/Symfony/Bundle/DependencyInjection/AppExtension.php
@@ -11,8 +11,8 @@ namespace CarlosChininin\App\Infrastructure\Symfony\Bundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class AppExtension extends Extension
 {
