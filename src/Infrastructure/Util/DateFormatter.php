c4b6d78b3bd2e2d3c9e7e2fb2d471340454123c2
Carlos Chininin <carloschininin@gmail.com>
2025-04-09 00:41:09 -0500

    fix Date Formatter in spanish

--- src/Infrastructure/Util/DateFormatter.php
+++ src/Infrastructure/Util/DateFormatter.php
@@ -4,25 +4,27 @@ namespace CarlosChininin\App\Infrastructure\Util;
 
 class DateFormatter
 {
+    private const FORMAT_MAP = [
+        'd' => 'dd', 'j' => 'd', 'm' => 'MM', 'n' => 'M',
+        'Y' => 'yyyy', 'y' => 'yy', 'F' => 'MMMM', 'M' => 'MMM',
+        'l' => 'EEEE', 'D' => 'EEE', 'H' => 'HH', 'G' => 'H',
+        'h' => 'hh', 'g' => 'h', 'i' => 'mm', 's' => 'ss',
+        'A' => 'a', 'a' => 'a'
+    ];
+
     public static function format(
         ?\DateTimeInterface $date,
-        string $pattern = 'd-m-Y',
+        string $format = 'd/m/Y',
         string $locale = 'es_PE',
-        $timezone = 'America/Lima',
+        string $timezone = 'America/Lima'
     ): ?string {
-        if (null === $date) {
-            return null;
-        }
-
-        $formatter = new \IntlDateFormatter(
+        return $date ? (new \IntlDateFormatter(
             $locale,
             \IntlDateFormatter::FULL,
             \IntlDateFormatter::FULL,
             $timezone,
             \IntlDateFormatter::GREGORIAN,
-            $pattern
-        );
-
-        return $formatter->format($date);
+            strtr($format, self::FORMAT_MAP)
+        ))->format($date) : null;
     }
 }
